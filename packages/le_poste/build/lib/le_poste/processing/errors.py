0efe5e9752ceed7df7ec62fb4d93fa9a3e5599c2
“Eashan123” <eashandeshmukh@gmail.com>
2020-07-29 21:10:14 +0530

    removed dist/build packages/leposte

--- packages/le_poste/build/lib/le_poste/processing/errors.py
+++
@@ -1,8 +0,0 @@
-class BaseError(Exception):
-    """Base package error."""
-
-
-class InvalidModelInputError(BaseError):
-    """Model input contains an error."""
-
-# This are some of the python best practices, check preprocessors regex function.
